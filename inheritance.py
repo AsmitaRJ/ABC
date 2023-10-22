1a38af0fcdae422e589b72c2b59a05baf579dee4
practise <asmita21jagadale@gmail.com>
2023-10-22 16:29:15 +0530

    first commit

---
+++ inheritance.py
@@ -0,0 +1,23 @@
+class Person:
+    def set_data(self, org, code):
+        self.org = org
+        self.code = code
+
+class Department(Person):
+    def get_data(self,dept,id):
+        self.dept = dept
+        self.id = id
+
+class Student(Department):
+    def set_name(self,name,sub):
+        self.name = name
+        self.sub = sub
+
+    def display(self):
+        print(f'Organization: {self.org} \nCode: {self.code} \nDepartment: {self.dept} \nID: {self.id} \nName: {self.name} \nSubject: {self.sub}')
+
+obj = Student()
+obj.set_data("MIT ADT University","0012")
+obj.get_data("Bioengineering","12")
+obj.set_name("Asmita", "System Biology")
+obj.display()
